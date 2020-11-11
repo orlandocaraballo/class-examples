639324d6ef1ef756e9563d751063cdc9218461ae
Orlando Caraballo <orlandocaraballo@gmail.com>
2020-11-11 18:41:53 -0500

    Added .vscode to .gitignore

--- dom/main.js
+++
@@ -1,8 +0,0 @@
-// const paragraph = document.createElement("p");
-
-// paragraph.innerText = "Hola!";
-
-// document.body.appendChild(paragraph);
-const elements = document.getElementsByTagName("h1");
-
-console.log(elements[0]);
