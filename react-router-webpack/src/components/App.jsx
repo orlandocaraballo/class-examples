451ccae4cbb4103cfaac44bb871a6bb468b2f7bd
Orlando Caraballo <orlandocaraballo@gmail.com>
2021-11-10 23:46:13 -0500

    Added style / css-loader for hot module reloading of css

--- react-router-webpack/src/components/App.jsx
+++ react-router-webpack/src/components/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import Footer from "./Footer.jsx";
 import Header from "./Header.jsx";
 import Main from "./Main.jsx";
+import "./App.css";
 
 const App = () => (
   <React.Fragment>
--- react-router-webpack/webpack.config.js
+++ react-router-webpack/webpack.config.js
@@ -21,6 +21,10 @@ module.exports = {
           presets: ["@babel/preset-react"],
         },
       },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"],
+      },
     ],
   },
   devServer: {
